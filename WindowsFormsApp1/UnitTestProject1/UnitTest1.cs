eee735b633aa26eeaea365c47c9c9c4d8a3c4df9
JoanJuan10 <iijoanjuan10@gmail.com>
2021-01-27 12:56:21 +0100

    Pruebas unitarias y parte grafica + Cuadrado

---
+++ WindowsFormsApp1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WindowsFormsApp1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void areacuadradoTest()
+        {
+            Geometria g = new Geometria();
+            int areacuadrado = g.areacuadrado(10);
+            Assert.AreEqual(100, areacuadrado);
+        }
+        [TestMethod]
+        public void areaCirculoTest()
+        {
+            Geometria g = new Geometria();
+            double areacirculo = g.areaCirculo(10);
+
+            const double PI = 3.1416;
+            double area = PI * Math.Pow(10, 2);
+
+            Assert.AreEqual(area, areacirculo);
+        }
+        [TestMethod]
+        public void areaTrianguloTest()
+        {
+            Geometria g = new Geometria(3);
+            double areatriangulo = g.areatriangulo(10,10);
+
+            Assert.AreEqual(50, areatriangulo);
+        }
+        [TestMethod]
+        public void areaRectanguloTest()
+        {
+            Geometria g = new Geometria();
+            double area = g.arearectangulo(10,10);
+
+            Assert.AreEqual(100, area);
+        }
+        [TestMethod]
+        public void areaPentagonoTest()
+        {
+            Geometria g = new Geometria();
+            double area = g.areapentagono(10, 10);
+
+            Assert.AreEqual(50, area);
+        }
+        [TestMethod]
+        public void arearomboTest()
+        {
+            Geometria g = new Geometria();
+            double area = g.arearombo(10, 10);
+
+            Assert.AreEqual(50, area);
+        }
+        [TestMethod]
+        public void arearomboide()
+        {
+            Geometria g = new Geometria();
+            double area = g.arearomboide(10, 10);
+
+            Assert.AreEqual(100, area);
+        }
+        [TestMethod]
+        public void areatrapecioTest()
+        {
+            Geometria g = new Geometria();
+            double area = g.areatrapecio(10, 10, 10);
+
+            Assert.AreEqual(100, area);
+        }
+    }
+}
+
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Cuadrado.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Cuadrado
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lado = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.lado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lado
+            // 
+            this.lado.Location = new System.Drawing.Point(232, 55);
+            this.lado.Name = "lado";
+            this.lado.Size = new System.Drawing.Size(164, 20);
+            this.lado.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(39, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Lado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(142, 125);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Cuadrado
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 194);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lado);
+            this.Name = "Cuadrado";
+            this.Text = "Cuadrado";
+            ((System.ComponentModel.ISupportInitialize)(this.lado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown lado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
